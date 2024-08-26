06c98a53bd99a99247d1d8e9334fcb0c94816569
Prajwal Tade <157635570+Prajwaltade-17@users.noreply.github.com>
2024-08-26 20:40:51 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,14 @@
+class literals{
+   public static void main(String[] args)
+{
+   byte age=20;
+   int no=3;
+   float f=5.2f;
+   long mob_no=51313103131L;
+   char ch='C';
+   double d=2.111;
+   boolean b=true;
+   String s="Flash";
+  System.out.println(s);
+}
+}
