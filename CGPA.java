98777ad4926c053bd17f5be9b8f3915759fe1fbc
Prajwal Tade <157635570+Prajwaltade-17@users.noreply.github.com>
2024-08-30 09:49:46 -0700

    Add files via upload

---
+++ CGPA.java
@@ -0,0 +1,14 @@
+class CGPA
+   {
+     public static void main(String args[])
+   {
+     float sub1=72;
+     float sub2=68;
+     float sub3=85;
+     float sub4=80;
+     float sub5=65;
+      float sum=sub1+sub2+sub3+sub4+sub5;
+     float CGPA=sum/50;
+     System.out.println(CGPA);
+}
+}
