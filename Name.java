ef964aafd75d637ac6f778bad3438e168737ac23
Prajwal Tade <157635570+Prajwaltade-17@users.noreply.github.com>
2024-09-01 10:53:09 -0700

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Name
+   {
+     public static void main(String args[])
+   {
+    Scanner sc= new Scanner(System.in);
+   System.out.println("What is your name");
+   String name=sc.next();
+   System.out.println("hello" + name + "have a good day");
+}
+}
