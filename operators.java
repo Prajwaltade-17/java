6b75045600176fc92dce755339490ad2ec3d2150
Prajwal Tade <157635570+Prajwaltade-17@users.noreply.github.com>
2024-09-08 09:46:22 -0700

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,16 @@
+class operators
+{
+  public static void main(String args[])
+  {
+  float a,b;
+    a=9;
+   b=6+a;
+  b+=3;
+System.out.println(b>10&&b<=15);
+System.out.println(b>12||b<10);
+
+ System.out.println(b>=18);
+System.out.println(b);
+
+}
+}
