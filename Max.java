b2d263b6bc4a443dbce9c7277cabfdd8f1af6050
Prajwal Tade <157635570+Prajwaltade-17@users.noreply.github.com>
2025-01-21 15:43:50 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Max
+{
+    public static void main(String[]args)
+    {
+        Scanner sc =new Scanner(System.in);
+       System.out.println("enter 1st number");
+       double num1=sc.nextDouble();
+       System.out.println("enter 2nd number");
+       double num2=sc.nextDouble();
+       double value =Math.max(num1,num2);
+       System.out.println("Max value:" +value);
+
+
+    }
+
+}
